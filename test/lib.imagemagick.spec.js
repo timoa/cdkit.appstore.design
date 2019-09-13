ffb3ce635c358b0a2114d147cfe552aed8d45549
Damien Laureaux <d.laureaux@timoa.com>
2019-09-13 10:23:29 +0200

    Extend Mocha timeout for GitHub Actions that can be higher than 2s

--- test/lib.imagemagick.spec.js
+++ test/lib.imagemagick.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable func-names */
 const { expect } = require('chai');
 const path = require('path');
 
@@ -7,24 +8,28 @@ const screenshotsPath = `${__dirname}/data/img`;
 
 // ImageMagick
 describe('ImageMagick library', () => {
-  describe('resizeScreenshot()', () => {
+  
+  describe('resizeScreenshot()', function () {
+    this.timeout(6000);
+    
     it('expect the "resizeScreenshot()" to be a function', (done) => {
       expect(lib.resizeScreenshot).to.be.a('function');
       done();
     });
     it('expect the "resizeScreenshot()" to not throw', () => {
       const destFilename = '1-iphone-5.8-test.png';
-      return lib.resizeScreenshot({
-        path: screenshotsPath,
-        filename: '1-iphone-5.8.png',
-        destFilename,
-        width: 591,
-        height: 1280
-      })
-      .then((result) => {
-        const destPath = path.join(`${__dirname}/../tmp/${destFilename}`);
-        expect(result).to.be.equals(destPath);
-      });
+      return lib
+        .resizeScreenshot({
+          path: screenshotsPath,
+          filename: '1-iphone-5.8.png',
+          destFilename,
+          width: 591,
+          height: 1280,
+        })
+        .then((result) => {
+          const destPath = path.join(`${__dirname}/../tmp/${destFilename}`);
+          expect(result).to.be.equals(destPath);
+        });
     });
   });
 
